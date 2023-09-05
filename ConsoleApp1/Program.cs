f7142d87067ca4182007abce2ae5ccc315e88a9f
Josue Rodriguez <josuemarce246@gmail.com>
2023-09-05 00:26:24 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+int a = 5;
+int b = a + 2; //OK
+
+bool test = true;
+
+// Error. Operator '+' cannot be applied to operands of type 'int' and 'bool'.
+int c = a + b;
+Console.WriteLine(c);
+
+
